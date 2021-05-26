51691d8922a2c04d8756a23e5143e88dfc755941
Aravinth Thangaraju <Aravinth_Thangaraju@Trimble.com>
2021-05-26 22:12:04 +0530

    added backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import Products from '../products'
-import Product from '../components/product'
+import React,{ useState, useEffect } from 'react'
+
 import { Row, Col} from 'react-bootstrap'
+import Product from '../components/product'
+import axios from 'axios'
 
 const HomeScreen = () => {
+    const [Products, setProducts] = useState([])
+    useEffect(() =>{
+
+       const fetchProducts = async() =>
+       {
+           const {data} = await axios.get('/api/products')
+           setProducts(data)
+    }
+    fetchProducts()
+}, [])
     return (
         <>
             <h1>Latest Products</h1>
