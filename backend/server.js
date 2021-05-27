22f37843359c1ba568eefbdbe47b1bc7237b9e1f
Aravinth Thangaraju <Aravinth_Thangaraju@Trimble.com>
2021-05-27 19:12:29 +0530

    added import

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import products from './data/products.js'
+import connectDB from './config/db.js'
+
+
 
 const app = express()
 dotenv.config()
+connectDB()
 
 
 
@@ -25,5 +30,5 @@ app.get('/api/products/:id',(req,res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, 
-    console.log(`Server running in ${process.env.ENVI} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.ENVI} mode on port ${PORT}`.yellow.bold)
     )
